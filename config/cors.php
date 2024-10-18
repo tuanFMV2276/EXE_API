37137829222e2a64033a40000f4c83566dd13326
tuanFMV2276 <tuanbqqe170037@fpt.edu.vn>
2024-10-18 23:11:29 +0700

    mupdate

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*', 'https://exe-201-fe.vercel.app'],
 
     'allowed_origins_patterns' => [],
 
